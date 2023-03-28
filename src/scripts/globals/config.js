f4a6529820a9bbeb58923688e0d5d4f2cb1d237e
Utomo Yoga <utomoyogafirmansyah@gmail.com>
2023-03-28 10:42:27 +0700

    add feature

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: "661ba38cd4ae39df958453ff15d32d94",
+  BASE_URL: "https://api.themoviedb.org/3/",
+  BASE_IMAGE_URL: "https://image.tmdb.org/t/p/w500/",
+  DEFAULT_LANGUAGE: "en-us",
+};
+
+export default CONFIG;
